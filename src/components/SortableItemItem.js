fd9a15afc49422a291bacff9895e71b9425e59a5
Robb-S <rsreg@bento.com>
2021-05-06 18:20:50 +0900

    UI for edit/update item.

--- src/components/SortableItemItem.js
+++ src/components/SortableItemItem.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 import {useSortable} from '@dnd-kit/sortable';
 import {CSS} from '@dnd-kit/utilities';
 import '../css/lists.css';
 import {useStore} from '../store/StoreContext';
 import {handleRemoveItem} from '../store/handlers';
 import { MdDragHandle } from 'react-icons/md';
-import { FiTrash2, FiEdit } from 'react-icons/fi';
+import { FiTrash2, FiEdit, FiCheckSquare, FiXCircle } from 'react-icons/fi';
 
 export function SortableItemItem(props) {
-  const {state, dispatch} = useStore();  
+  const {state, dispatch} = useStore();
+  const [editMode, setEditMode] = useState(false);  // set edit mode when edit button is pressed.
+  const [itemName, setItemName] = useState(props.item.itemName);
+  const [itemNote, setItemNote] = useState(props.item.itemNote);
 
   const { attributes, listeners, setNodeRef, transform, transition } = 
     useSortable({id: props.id});
@@ -23,34 +26,91 @@ export function SortableItemItem(props) {
     handleRemoveItem(itemID, state, dispatch);
   }
 
+  const setupEdit = () => {
+    setEditMode(true);
+  }
+
   const editItem = (itemID) => {
+    setEditMode(false);
     // handleEditItem(itemID, state, dispatch);
   }
 
+  const cancelEdit = () => {
+    setEditMode(false);
+    setItemName(props.item.itemName);
+    setItemNote(props.item.itemNote);
+  }
+
   return (
-    <tr ref={setNodeRef} style={style} className='oneItemLine'>
-      <td className='dragHandle'>
-        <span className='iconBorderSmall iconHandle'>
-          <MdDragHandle {...listeners} {...attributes} size='16' color='red' />
-        </span>
-      </td>
-      <td className='itemName'>
-        {props.item.itemName}
-      </td>
-      <td className='itemNote'>
-        {props.item.itemNote}  
-      </td>
-      <td className='buttons'>
-        <span className='iconDelete iconBorder'>
-          <FiTrash2 onClick={() => removeItem(props.item.id)}
-          className='iconBorder' size='24' color='#555555' />
-        </span>
-        <span className="sliver5"> </span>
-        <span className='iconEdit iconBorder'>
-          <FiEdit onClick={() => editItem(props.item.id)}
-          className='iconBorder' size='24' color='#555555' />
-        </span>
-      </td>
-    </tr>
+    <Fragment>
+      { !editMode && 
+      <Fragment>
+        <tr ref={setNodeRef} style={style} className='oneItemLine'>
+          <td className='dragHandle'>
+            <span className='iconBorderSmall iconHandle'>
+              <MdDragHandle {...listeners} {...attributes} 
+              title='drag to change order' size='16' color='green' />
+            </span>
+          </td>
+          <td className='itemName'>
+            {props.item.itemName}
+          </td>
+          <td className='itemNote'>
+            {props.item.itemNote}  
+          </td>
+          <td className='buttons'>
+            <span className='iconEdit iconBorder'>
+              <FiEdit onClick={() => setupEdit()}
+              title='edit item' className='iconBorder' size='24' color='#555555' />
+            </span>
+            <span className="sliver5"> </span>
+            <span className='iconDelete iconBorder'>
+              <FiTrash2 onClick={() => removeItem(props.item.id)}
+              title='delete item' className='iconBorder' size='24' color='#555555' />
+            </span>
+          </td>
+        </tr>
+      </Fragment>
+      }
+
+      { editMode && 
+      <Fragment>
+        <tr>
+          <td></td>
+          <td colSpan="2" className='editFormTD'>
+            <form className='editFormForm'>
+              <span className='editInputArea'>
+                <input
+                  value={itemName}
+                  onChange={(e) => setItemName(e.target.value)}
+                  type="text"
+                />
+                <span className="sliver5"> </span>
+                <input
+                  value={itemNote}
+                  onChange={(e) => setItemNote(e.target.value)}
+                  type="text"
+                />
+              </span>
+            </form>
+          </td>
+          <td className='buttons editButtons'>
+            <span className='editButtonArea'>
+              <span className='iconCheckmark iconNoBorder'>
+                <FiCheckSquare onClick={() => editItem(props.item.id)}
+                title='accept edit' size='24' color='#555555' />
+              </span>
+              <span className="sliver5"> </span>
+              <span className='iconEdit iconNoBorder'>
+                <FiXCircle onClick={() => cancelEdit()}
+                title='cancel edit' className='iconBorder' size='24' color='#555555' />
+              </span>
+            </span>
+          </td>
+        </tr>
+      </Fragment>  
+      }
+
+    </Fragment>
   );
 }
