9411c41330e79df14870fc317614455654d8a843
Robb Satterwhite <rsreg@bento.com>
2021-08-05 23:22:28 +0900

    Use Material UI for registration form.

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -6,6 +6,9 @@ import { useDebounce } from '../util/helpers';
 import { useHistory, Link } from 'react-router-dom';
 import { userExistsAPI } from '../store/apiCalls';
 import * as api from '../util/constants';
+import { FormLabel } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import { IconButton } from './IconButton';
 
 const Registration = () => {
   const { state, dispatch } = useStore();
@@ -59,8 +62,17 @@ const Registration = () => {
     console.log('test button end. ');
   };
 
-  const onSubmitReg = async (e) => {
+
+  const onSubmitForm = (e) => { // called when you press Enter after typing
     e.preventDefault();
+    onSubmitButton();
+  };
+
+  /**
+   * Send values of text fields to handler for new-user registration.
+   * TODO: check if passwords match, if not, use error message area for message.
+   */
+  const onSubmitButton = async () => { // called when you press button
     if ((userName.length===0) || (userPwd.length===0)) {return;}
     const userInfo = { username: userName, password: userPwd,
       password2: userPwd2, email: userEmail, first_name: userFirstName,
@@ -72,96 +84,135 @@ const Registration = () => {
     else setRegMsg(regResult);
   };
 
-  // # BASE_URL/isuser/admin/ pattern
-  const onChangeUserName = async (e) => {
-    const uName = e.target.value;
-    console.log('length: ' + uName.length);
-    setUserName(uName);
+  // const onSubmitReg = async (e) => {
+  //   e.preventDefault();
+  //   if ((userName.length===0) || (userPwd.length===0)) {return;}
+  //   const userInfo = { username: userName, password: userPwd,
+  //     password2: userPwd2, email: userEmail, first_name: userFirstName,
+  //     last_name: userLastName, nickname: userNickname };
+  //   const regResult = await handleReg(userInfo, dispatch);
+  //   console.log(regResult);
+  //   // handleReg(userInfo, dispatch);
+  //   if (regResult===api.OK) { history.push('/'); }
+  //   else setRegMsg(regResult);
+  // };
+
+  // // # BASE_URL/isuser/admin/ pattern
+  // const onChangeUserName = async (e) => {
+  //   const uName = e.target.value;
+  //   console.log('length: ' + uName.length);
+  //   setUserName(uName);
+  // };
+
+  const headingArea = () => {
+    return (
+      <div className='headingZone loginHeading'>
+        <div className='headingNameArea'>
+          Register new user
+        </div>
+        <div className='headingIcons'>
+
+        </div>
+      </div>
+    );
+  };
+  
+  const regForm = () => {
+    const regConfig = {
+      caption:'register', title:'register new user data', iconType:'login', callProc:onSubmitButton,
+    };
+    return (
+      <Fragment>
+        <form className='loginForm' onSubmit={onSubmitForm}>
+          <FormLabel component="legend"><div className='formlabel'>
+          Please enter the fields below
+            or <Link to={{ pathname: '/login/' }}>log in (for existing users)</Link>
+          </div>
+          </FormLabel>
+          <div className='reg1 regbox'>
+            <TextField
+              required
+              label="User name:" value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              variant='outlined'
+              margin='dense'
+              inputProps={{ autoCapitalize: 'off', autoComplete:'new-password' }}
+            />
+          </div>
+          <div className='userNameMessage'>
+            {uNameMsg}
+          </div>
+          <div className='reg2 regbox'>
+            <TextField
+              required
+              label='Password:' value={userPwd}
+              onChange={(e) => setUserPwd(e.target.value)}
+              variant='outlined'
+              margin='dense'
+              inputProps={{ autoCapitalize: 'off', autoComplete:'off' }}
+            />
+          </div>
+          <div className='reg3 regbox'>
+            <TextField
+              required
+              label='Repeat password:' value={userPwd2}
+              onChange={(e) => setUserPwd2(e.target.value)}
+              variant='outlined'
+              margin='dense'
+              inputProps={{ autoCapitalize: 'off', autoComplete:'off' }}
+            />
+          </div>
+          <div className='reg4 regbox'>
+            <TextField
+              required
+              label='Email:' value={userEmail}
+              onChange={(e) => setUserEmail(e.target.value)}
+              variant='outlined'
+              margin='dense'
+              inputProps={{ autoCapitalize: 'off' }}
+            />
+          </div>
+          <div className='reg5 regbox'>
+            <TextField
+              label='First name:' value={userFirstName}
+              onChange={(e) => setUserFirstName(e.target.value)}
+              variant='outlined'
+              margin='dense'
+              inputProps={{ autoCapitalize: 'off' }}
+            />
+          </div>
+          <div className='reg6 regbox'>
+            <TextField
+              label='Last name:' value={userLastName}
+              onChange={(e) => setUserLastName(e.target.value)}
+              variant='outlined'
+              margin='dense'
+              inputProps={{ autoCapitalize: 'off' }}
+            />
+          </div>
+          <div className='reg7 regbox'>
+            <TextField
+              label='Nickname:' value={userNickname}
+              onChange={(e) => setUserNickname(e.target.value)}
+              variant='outlined'
+              margin='dense'
+              inputProps={{ autoCapitalize: 'off' }}
+            />
+          </div>
+          <div className='regButton'><IconButton config={ regConfig } /></div>
+          <input type="submit" className="hidden" />
+        </form>
+      </Fragment>
+    );
   };
 
   return (
     <Fragment>
-        <div className='additem'>
-          <div className='msgPanel'>{regMsg}</div>
-          <br />
-          <form onSubmit={onSubmitReg}>
-            <label>User name: </label>
-            <input
-              value={userName}
-              onChange={onChangeUserName}
-              type="text"
-              placeholder="user name"
-            />
-            <br />
-            {uNameMsg}
-            <br /><br />
-            <label> Password: </label>
-            <input
-              value={userPwd}
-              onChange={(e) => setUserPwd(e.target.value)}
-              type="text"
-              placeholder="password"
-            />
-            <br /><br />
-            <label> Repeat password: </label>
-            <input
-              value={userPwd2}
-              onChange={(e) => setUserPwd2(e.target.value)}
-              type="text"
-              placeholder="repeat password"
-            />
-
-            <br /><br />
-            <label> First name: </label>
-            <input
-              value={userFirstName}
-              onChange={(e) => setUserFirstName(e.target.value)}
-              type="text"
-              placeholder="first name"
-            />
-
-            <br /><br />
-            <label> Last name: </label>
-            <input
-              value={userLastName}
-              onChange={(e) => setUserLastName(e.target.value)}
-              type="text"
-              placeholder="last name"
-            />
-
-            <br /><br />
-            <label> Nickname: </label>
-            <input
-              value={userNickname}
-              onChange={(e) => setUserNickname(e.target.value)}
-              type="text"
-              placeholder="nickname"
-            />
-
-            <br /><br />
-            <label> Email: </label>
-            <input
-              value={userEmail}
-              onChange={(e) => setUserEmail(e.target.value)}
-              type="text"
-              placeholder="email"
-            />
-            <br /><br />
-            <button>Register</button>
-          </form>
-        </div>
-        <button
-          className="btn default-btn hidden"
-           onClick={() => onTestButton()}
-        >
-          Test Button
-        </button><br />
-
-        <div className='redirectLink'>
-          <Link to={{ pathname: '/login/' }}>
-            Or log in (for existing users)
-          </Link>
-        </div>
+      <div className='mainContainer'>
+        <div className='topLogo'>- Cross It Off the List -</div>
+        { headingArea() }
+        { regForm() }
+      </div>
     </Fragment>
   );
 };
