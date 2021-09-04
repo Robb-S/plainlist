90308215dea742edc7dae7ea14bffed644a76a92
Robb Satterwhite <rsreg@bento.com>
2021-09-04 23:03:43 +0900

    help for copying list

--- src/components/Help.js
+++ src/components/Help.js
@@ -31,7 +31,7 @@ const Help = () => {
           When you first start using the app your lists will be organized in Hierarchical mode, with two starter categories ("Shopping" and "To do") and one starter list in each category.  You can rename any of these, and add or delete categories and lists.
         </div>
         <div className='helpHeader'>
-          Switching between Hierarchical and Flat modes
+          Switch between Hierarchical and Flat modes
         </div>
         <div className='helpPara'>
           You can switch between Hierarchical and Flat modes at any time by going to the Settings page.  (Press the cog icon from any page to reach the Settings page.)
@@ -43,13 +43,19 @@ const Help = () => {
           The app will remember the most recent list where you added, deleted or edited an item, and by default it will display this list when you restart the app.  If you would prefer to show the top page when you restart the app, you can turn off this feature from the settings page.
         </div>
         <div className='helpHeader'>
-          Reordering lists with drag and drop
+          Reorder lists with drag and drop
         </div>
         <div className='helpPara'>
           If you want to change the order of categories, lists, or items, use the six-dot icon to the left of the text to drag the item to the position you want, either with the mouse (on desktop) or with your finger (on a touch device).
         </div>
         <div className='helpHeader'>
-          Switching categories for a list
+          Copy a list
+        </div>
+        <div className='helpPara'>
+          If you want to create a copy of an existing list and all its items, go to the list page, press the "Changes to List" button, then press "Copy This List."  You can edit the name of the new list, then press "Copy Current List" to create the new list.
+        </div>
+        <div className='helpHeader'>
+          Switch categories for a list
         </div>
         <div className='helpPara'>
           When you are on the page for a particular list, you can change the category of that list by pressing the "Change Category" icon and selecting a different category.
@@ -58,7 +64,7 @@ const Help = () => {
           This can be useful if you have added a new list when in Flat mode, but have switched back to Hierarchical mode, as the new list will have been placed in an "Uncategorized" category.  Simply move it to whichever other category you prefer, or create a new category.
         </div>
         <div className='helpHeader'>
-          Installation on Android devices
+          Install on an Android device
         </div>
         <div className='helpPara'>
           1. Type the URL of this site into your device's browser window and press enter.<br />
